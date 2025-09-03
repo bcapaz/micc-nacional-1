ecf1c83e19e1d6b461f112699df997188c3c19ac
bcapaz <bmcapaz@gmail.com>
2025-09-03 15:11:28 -0300

    Create utils.ts

---
+++ server/utils.ts
@@ -0,0 +1,20 @@
+import { scrypt, randomBytes, timingSafeEqual } from "crypto";
+import { promisify } from "util";
+
+const scryptAsync = promisify(scrypt);
+
+// Função para criar o hash de uma password
+export async function hashPassword(password: string) {
+  const salt = randomBytes(16).toString("hex");
+  const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+  return `${buf.toString("hex")}.${salt}`;
+}
+
+// Função para comparar uma password fornecida com uma armazenada
+export async function comparePasswords(supplied: string, stored: string) {
+  if (!stored || !stored.includes('.')) return false;
+  const [hashed, salt] = stored.split(".");
+  const hashedBuf = Buffer.from(hashed, "hex");
+  const suppliedBuf = (await scryptAsync(supplied, salt, 64)) as Buffer;
+  return timingSafeEqual(hashedBuf, suppliedBuf);
+}
